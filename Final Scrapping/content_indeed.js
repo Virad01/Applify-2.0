ab24c0b2246b19a301810cc1ab3767aa6b607f86
vaniseth <74897823+vaniseth@users.noreply.github.com>
2024-01-29 01:48:19 +0530

    getting indeed content

---
+++ Final Scrapping/content_indeed.js
@@ -0,0 +1,5 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "scrapeJobDetails") {
+      chrome.runtime.sendMessage({ action: 'scrapeJobDetails', website: 'indeed.com' });
+    }
+  });
