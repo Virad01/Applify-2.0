21e6aeeee21b3845fb67b0bbdc1089f372514754
VinayakRaoDikshit <vinayakrdikshit@gmail.com>
2024-03-05 15:27:02 +0530

    Add scraping code for Indeed

--- Current Scraping/scraperApp.py
+++ Current Scraping/scraperApp.py
@@ -42,6 +42,45 @@ def scrape_LinkedIn(jobUrl):
         print(type(p1))
         dict={'jobTitle':job_title.text,'companyName': company_name.text, 'date':Date_app }
         return(dict)
+    
+def scrape_LinkedIn(jobUrl):
+
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("debuggerAddress", "localhost:9222")
+    driver=webdriver.Chrome(options=options)
+    driver.get(url=jobUrl)
+    if(jobUrl.startswith("https://www.linkedin.com/jobs/collections/")): 
+        job_title=driver.find_element(By.XPATH,"//h2[@class='t-24 t-bold job-details-jobs-unified-top-card__job-title']")
+
+        company_name=driver.find_element(By.XPATH,"//div[@class='job-details-jobs-unified-top-card__primary-description-without-tagline mb2']//a[@class='app-aware-link ']")
+        
+        dict={'jobTitle':job_title.text,'companyName': company_name.text, 'date':Date_app }
+        return(dict)
+
+    elif(jobUrl.startswith("https://www.linkedin.com/jobs/view/")):
+        time.sleep(5)
+        job_title=driver.find_element(By.XPATH,"//h1[@class='t-24 t-bold job-details-jobs-unified-top-card__job-title']")
+
+        company_name=driver.find_element(By.XPATH,"//div[@class='job-details-jobs-unified-top-card__primary-description-without-tagline mb2']//a[@class='app-aware-link ']")
+        # driver.quit()
+        p1=job_title.text
+        print(type(p1))
+        dict={'jobTitle':job_title.text,'companyName': company_name.text, 'date':Date_app }
+        return(dict)
+
+def scrape_indeed(jobUrl):
+
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("debuggerAddress", "localhost:9222")
+    driver=webdriver.Chrome(options=options)
+    driver.get(url=jobUrl)
+    job_title=driver.find_element(By.XPATH,"//h2[@class='jobsearch-JobInfoHeader-title css-jf6w2g e1tiznh50']")
+
+    company_name=driver.find_element(By.XPATH,"//span[@class='css-1saizt3 e1wnkr790']")
+        
+    dict={'jobTitle':job_title.text,'companyName': company_name.text, 'date':Date_app }
+    return(dict)
+
 
 @app.route('/scrape', methods=['GET'])
 def scrape_job_details():
@@ -52,8 +91,9 @@ def scrape_job_details():
         job_details = scrape_LinkedIn(website)
         return(job_details)
         
-    # elif website.startswith("indeed.com"):
-    #     job_details = scrape_indeed()
+    elif website.startswith("https://in.indeed.com/"):
+        job_details = scrape_indeed(website)
+        return(job_details)
     # else:
     #     return jsonify({'error': 'Invalid website'})
 if __name__ == '__main__':
